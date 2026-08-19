cd523db7cd7aeb1e07ca58e3d63a0b80e7c1d694
I-love-Giri <hello.mayankgiri@gmail.com>
2026-08-19 23:58:08 +0530

    feat: integrate story generation workflow

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from dotenv import load_dotenv
-import os
-from openai import OpenAI
-
-load_dotenv()
-
-api_key = os.getenv("OPENROUTER_API_KEY")
-
-client = OpenAI(base_url="https://openrouter.ai/api/v1", api_key=api_key)
-
-# story_idea = input("What kind of story do you want? ")
+from character_manager import get_characters
+from story_generator import generate_story
+from config import client
+from story_planner import create_story_plan
 
 
 def get_story_idea():
@@ -16,27 +9,18 @@ def get_story_idea():
     return story_idea
 
 
-def generate_story(story_idea):
-    response = client.chat.completions.create(
-        model="openrouter/free",
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a professional horror story writer. Write atmospheric and suspenseful stories.",
-            },
-            {
-                "role": "user",
-                "content": f"Write a short and scary story based on this idea: {story_idea}",
-            },
-        ],
-    )
-
-    return response.choices[0].message.content
-
-
 if __name__ == "__main__":
     story_idea = get_story_idea()
-    story = generate_story(story_idea)
+    story_plan = create_story_plan(client, story_idea)
 
+    characters = get_characters(story_plan)
+
+    for character in characters:
+        print("Name: ", character["name"])
+        print("Role: ", character["role"])
+        print("Description: ", character["description"])
+        print()
+
+    """story = generate_story(client, story_plan)
     print("\n--- YOUR STORY ---\n")
-    print(story)
+    print(story)"""
