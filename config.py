091989cd34661603c50b045b2819492ca3f6d682
I-love-Giri <hello.mayankgiri@gmail.com>
2026-08-19 23:57:09 +0530

    feat: add application configuration

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+api_key = os.getenv("OPENROUTER_API_KEY")
+
+client = OpenAI(base_url="https://openrouter.ai/api/v1", api_key=api_key)
