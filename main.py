8a20939f57ecba31d3e22905b0cb2bfbbe4c1b9a
I-love-Giri <hello.mayankgiri@gmail.com>
2026-08-20 23:59:26 +0530

    feat: integrate story memory workflow

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from character_manager import get_characters
 from story_generator import generate_story
 from config import client
+from story_memory import update_memory
 from story_planner import create_story_plan
+import json
 
 
 def get_story_idea():
@@ -15,12 +17,24 @@ if __name__ == "__main__":
 
     characters = get_characters(story_plan)
 
-    for character in characters:
+    """for character in characters:
         print("Name: ", character["name"])
         print("Role: ", character["role"])
         print("Description: ", character["description"])
-        print()
+        print()"""
 
-    """story = generate_story(client, story_plan)
-    print("\n--- YOUR STORY ---\n")
+    story = generate_story(client, story_plan, characters)
+    memory = update_memory(client, story, characters)
+    memory = json.loads(memory)
+
+    """print("\n--- YOUR STORY ---\n")
     print(story)"""
+
+    print("\n--- MEMORY TYPE ---\n")
+    print(type(memory))
+
+    print("\n--- UPDATED MEMORY ---\n")
+    print(memory)
+
+    print("\n--- FIRST CHARACTER ---")
+    print(memory[0])
